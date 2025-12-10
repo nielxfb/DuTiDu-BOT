4403edd1dd27e663967a5b13712a1c26c20e79d9
nielxfb <adamludnl@gmail.com>
2025-12-10 09:05:28 +0700

    feat: additional commands for rang

--- util/commands.py
+++ util/commands.py
@@ -4,20 +4,9 @@ from controllers.help import handle_help
 from controllers.rang import handle_rang
 
 commands = [
-    {
-        "aliases": ["/jadwal", "/schedule"],
-        "handler": handle_schedule
-    },
-    {
-        "aliases": ["/shift", "/sh"],
-        "handler": handle_shift
-    },
-    {
-        "aliases": ["/help"],
-        "handler": handle_help
-    },
-    {
-        "aliases": ["/rang"],
-        "handler": handle_rang
-    }
+    {"aliases": ["/jadwal", "/schedule"], "handler": handle_schedule},
+    {"aliases": ["/shift", "/sh"], "handler": handle_shift},
+    {"aliases": ["/help"], "handler": handle_help},
+    {"aliases": ["rang", "Rang"], "handler": handle_rang},
 ]
+
